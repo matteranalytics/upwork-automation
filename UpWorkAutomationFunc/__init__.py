303718a2969d07d56a96e4d1841dd9bc4f4425c4
Yianni Mercer <yianni@matteranalytics.io>
2024-01-15 13:19:23 -0600

    Updated init function

--- UpWorkAutomationFunc/__init__.py
+++ UpWorkAutomationFunc/__init__.py
@@ -2,6 +2,7 @@ import logging
 from push_to_airtable import push_to_airtable
 import azure.functions as func
 
+
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
